cd2730c9457f66de9704a0d4aea1c4b1f99d6d9a
Ahmed <ahmed.emad.edu@gmail.com>
2024-11-26 20:14:46 +0300

    UserDataAO and UserService

--- src/UserDataAO.java
+++ src/UserDataAO.java
@@ -3,56 +3,20 @@ import java.util.ArrayList;
 import java.util.List;
 public class UserDataAO {
 
-    public User login(String username, String password) throws SQLException {
-        String query = "Select * from users where username = ? and password = ?";
-        PreparedStatement stmt = DBconnector.conn.prepareStatement(query);
-        stmt.setString(1,username);
-        stmt.setString(2,password);
-        ResultSet resultSet = stmt.executeQuery();
-        while(resultSet.next())
-        {
-            int user_id = resultSet.getInt("user_id");
-            String email = resultSet.getString("email");
-            String userType = resultSet.getString("role");
-
-            if(userType.equalsIgnoreCase("Admin"))
-            {
-                Admin admin = new Admin(user_id,username,password,email,userType);
-                return admin;
-            }
-            else if(userType.equalsIgnoreCase("Member"))
-            {
-                Member member = new Member(user_id,username,password,email,userType);
-                return member;
-            }
-
-        }
-        return null;
-    }
-
     public void updatePassword(int userId, String oldPassword, String newPassword) throws SQLException {
         String query1 = "Select password from users where userId = ?";
         PreparedStatement stmt = DBconnector.conn.prepareStatement(query1);
         ResultSet resultSet1 = stmt.executeQuery();
         if(resultSet1.next()){
-            if(oldPassword.equalsIgnoreCase(resultSet1.getString("password"))){
                 String query2 = "Update users set password = ? where usersId = "+userId;
                 PreparedStatement stmt2 = DBconnector.conn.prepareStatement(query2);
                 stmt2.setString(1,newPassword);
                 int resultSet2 = stmt2.executeUpdate();
                 System.out.println("Password changed successfully!");
-            }
-            else if(!(oldPassword.equalsIgnoreCase(resultSet1.getString("password"))))
-                System.out.println("Error, the old password you provided doesn't match the current password!");
         }
     }
 
     public void updateUsername(int userId, String oldUsername, String newUsername) throws SQLException {
-        if(getUserByUsername(newUsername) != null)
-        {
-            System.out.println("The new username already exists!");
-        }
-        else {
             String query1 = "Select username from users where userId = ?";
             PreparedStatement stmt = DBconnector.conn.prepareStatement(query1);
             ResultSet resultSet1 = stmt.executeQuery();
@@ -65,17 +29,10 @@ public class UserDataAO {
                     System.out.println("Username changed successfully!");
                 } else if (!(oldUsername.equalsIgnoreCase(resultSet1.getString("username"))))
                     System.out.println("Error, the old username you provided doesn't match the current username!");
-            }
         }
     }
 
     public void updateEmail(int userId, String oldEmail, String newEmail) throws SQLException {
-        if(getUserByEmail(newEmail) != null)
-        {
-            System.out.println("The new email already exists!");
-        }
-
-        else {
             String query1 = "Select email from users where userId = ?";
             PreparedStatement stmt = DBconnector.conn.prepareStatement(query1);
             ResultSet resultSet1 = stmt.executeQuery();
@@ -88,7 +45,6 @@ public class UserDataAO {
                     System.out.println("Email changed successfully!");
                 } else if (!(oldEmail.equalsIgnoreCase(resultSet1.getString("email"))))
                     System.out.println("Error, the old email you provided doesn't match the current email!");
-            }
         }
     }
 
@@ -100,22 +56,14 @@ public class UserDataAO {
     }
 
     public void addUser(String username, String password, String email, String userType) throws SQLException {
-
-        if(getUserByUsername(username) != null){
-            System.out.println("Username already exists!");
-        }
-
-        else {
             String query = "insert into users (username, password, email, role) VALUES (?,?,?,?)";
             PreparedStatement stmt = DBconnector.conn.prepareStatement(query);
             stmt.setString(1, username);
             stmt.setString(2, password);
             stmt.setString(3, email);
             stmt.setString(4, userType);
-
             int resultSet = stmt.executeUpdate();
             System.out.println("User added successfully!");
-        }
     }
 
     public User getUserByUsername(String username) throws SQLException {
@@ -212,18 +160,5 @@ public class UserDataAO {
             }
         }
         return users;
-    }
-
-    public boolean authenticateUser(String username, String password) throws SQLException {
-        String query = "Select * from users where username = ? and password = ?";
-        PreparedStatement stmt = DBconnector.conn.prepareStatement(query);
-        stmt.setString(1,username);
-        stmt.setString(2,password);
-        ResultSet resultSet = stmt.executeQuery();
-        while(resultSet.next())
-        {
-            return true;
-        }
-        return false;
     }
 }
